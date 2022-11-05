01741c484240d4e85faa6b9f8bcd919ff2bccd86
Rodolfo Fernando Juarez <rfjuarez@gmail.com>
2022-11-05 12:19:44 -0300

    feat(Paragraph): replace p for div

--- src/ui/components/Text/Paragraph/Paragraph.styles.js
+++ src/ui/components/Text/Paragraph/Paragraph.styles.js
@@ -2,7 +2,7 @@ import {stylesTextBuilder} from "../../../../assets/styles/text-styles";
 import styled, {css} from 'styled-components';
 
 
-export const Paragraph = styled.p`
+export const Paragraph = styled.div`
     padding: 8px 0px;
     margin: 0;
     text-overflow: ellipsis;
